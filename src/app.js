adcc928164217e162914755c3070150bd18f291f
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-04-09 19:49:33 +0000

    final result

--- src/app.js
+++ src/app.js
@@ -4,19 +4,44 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-//
-let pokerPals = ["diamond", "club", "heart", "spade"]
+//arrays//
+const pokerPals = ["♦", "♣", "♥", "♠"]
 const cardVariables = ["A", "2", "3", "4", "5", "6", "7", "8", "9","J", "Q", "K"]
-
+//funcion general//
+function myRandomCard () {
+//funcion para generar indices random//
 function getRandomNumber (arr) {
   let randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
 };
+//mezcla el array del pal y define los colores para la clase//
+function mixCards (pokerPals) {
+  if (pokerPals === "♥" || pokerPals === "♦") {
+    return 'red';
+  } 
+  if(pokerPals === "♠" ||  pokerPals === "♣") {
+    return 'black';
+  }
+  }
+//variables de la logica//
+  const randomCardNumber = getRandomNumber(cardVariables);
+  const randomPal = getRandomNumber (pokerPals); 
+  const randomPalGroup = mixCards(randomPal);
+
+//manipulacion del DOM//
+  document.getElementById('card-number1').innerText = randomCardNumber;
+  document.getElementById('figures-up').innerText = randomPal;
+  document.getElementById('figures-down').innerText = randomPal;
+  const palUp = document.querySelector(".pal-up");
+  const palDown = document.querySelector(".pal-down");
+  palUp.classList.remove('red', 'black');
+  palDown.classList.remove('red', 'black');
+  palUp.classList.add(randomPalGroup);
+  palDown.classList.add(randomPalGroup);
+}
 
 window.onload = function() {
-
-  
-  //write your code here
-  console.log (getRandomNumber[cardVariables]);
-
+  document.getElementById('button-card').addEventListener('click', (e) => {
+    myRandomCard();
+  });
 };
